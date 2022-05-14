245767982387757818bcbdfa3dc13fd690352812
Cesarsp41 <64189944+Cesarsp41@users.noreply.github.com>
2022-05-14 17:35:15 -0500

    Update Practica5.py

--- Practica5.py
+++ Practica5.py
@@ -1,3 +1,14 @@
 import cv2
 import numpy as np
 
+imagen = cv2.imread("Bandera.jpg")
+gris = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY) #Grises
+
+cv2.imshow("Original",imagen)
+cv2.moveWindow("Original",0,300)
+
+cv2.imshow("Blanco y negro",gris)
+cv2.moveWindow("Blanco y negro",1200,300)
+
+opc = cv2.waitKey(0)
+
